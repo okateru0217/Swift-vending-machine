af43014da21dadc0e7b6dcc832dadd094cd942b6
okateru0217 <okateru.0217@gmail.com>
2021-03-06 12:45:29 +0900

    first commit

---
+++ VendingMachineApp/VendingMachineApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VendingMachineApp
+//
+//  Created by 岡澤輝明 on 2021/03/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
