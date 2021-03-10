a609a078b66e7ddabc567d55b0262fc3602e6b30
okateru0217 <okateru.0217@gmail.com>
2021-03-10 21:08:14 +0900

    Add:「おつりボタン」押下時の処理を追加
    お釣りの表示と釣り銭残高を減らす処理の追加

--- VendingMachineApp/VendingMachineApp/Controller/MoneyManagement.swift
+++ VendingMachineApp/VendingMachineApp/Controller/MoneyManagement.swift
@@ -14,6 +14,7 @@ class ChangeManagement: ViewController {
     var oneHundredYenStock = 100
     var fiveHundredYenStock = 10
     
+    // 釣り銭を補充する関数
     func increaseChange(increaseMoney: putMoneyButtonTag) {
         switch increaseMoney {
         case .tenYen:
@@ -24,6 +25,39 @@ class ChangeManagement: ViewController {
             oneHundredYenStock += 1
         case .fiveHundredYen:
             fiveHundredYenStock += 1
+        }
+    }
+    
+    var putOutTotalPutMoney = 0
+    // おつり分の釣り銭残高を減らす関数
+    func putOutChange(totalAddPutMoney: Int) {
+        putOutTotalPutMoney = totalAddPutMoney
+        print(putOutTotalPutMoney)
+        while putOutTotalPutMoney >= 0 {
+            if putOutTotalPutMoney >= 500 {
+                putOutTotalPutMoney -= 500
+                fiveHundredYenStock -= 1
+                continue
+            }
+            if putOutTotalPutMoney >= 100 {
+                putOutTotalPutMoney -= 100
+                oneHundredYenStock -= 1
+                continue
+            }
+            if putOutTotalPutMoney >= 50 {
+                putOutTotalPutMoney -= 50
+                fiftyYenStock -= 1
+                continue
+            }
+            if putOutTotalPutMoney >= 10 {
+                putOutTotalPutMoney -= 10
+                tenYenStock -= 1
+                continue
+            }
+            if putOutTotalPutMoney < 10 {
+                putOutTotalPutMoney -= putOutTotalPutMoney
+                break
+            }
         }
     }
 }
